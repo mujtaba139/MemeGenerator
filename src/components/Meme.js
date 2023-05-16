88e502f5d78283b00ded75c71ea902c9779ae270
MUJTABA <muji.khan26@gmail.com>
2023-05-16 12:41:37 +0500

    Added text functionality

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 
 export default function Meme() {
   const [memeImage, setMemeImage] = useState("");
-
+  const[topText , setToptext] = useState('');
+  const[bottomText , setBottomtext] = useState('');
+  
   async function getMemeimage() {
     const response = await fetch("https://api.imgflip.com/get_memes");
     const meme = await response.json();
@@ -12,21 +14,37 @@ export default function Meme() {
     setMemeImage(url);
     console.log(memesArray[5])
   }
+  function handleToptext(event){
+    setToptext(event.target.value);
+  }
+  function handleBottomtext(event){
+    setBottomtext(event.target.value);
+  }
 
   return (
     <div className="main">
       <div className="form">
-        <input type="text" className="form-input" placeholder="Top Text" />
         <input
+        type="text" 
+        className="form-input"
+         placeholder="Top Text"
+          onChange={handleToptext}
+           />
+        <input
+        onChange={handleBottomtext}
           type="text"
           className="form-input"
           placeholder="Bottom Text"
         />
         <button onClick={getMemeimage} className="form-button">
-          Generate a new image
+          Generate a new image :D
         </button>
       </div>
+      <div className="meme">
       <img className="generate-image" src={memeImage} alt="text"></img>
+      <h2 className="meme-text top">{topText}</h2>
+      <h2 className="meme-text bottom">{bottomText}</h2>
+      </div>
     </div>
   );
 }
